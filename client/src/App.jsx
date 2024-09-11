fc99008e80ae9e291329aef78540db255c41dce0
Siddharth Dhanasekar <siddudhanasekar@gmail.com>
2024-09-11 15:06:23 -0400

    CORS policy fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ import AccountPage from "./pages/AccountPage";
 import PlacePage from "./pages/PlacePage";
 import BookingPage from "./pages/BookingPage";
 
-axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
 function App() {
